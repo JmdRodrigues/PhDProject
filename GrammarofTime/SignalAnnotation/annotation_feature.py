376fd6ce7c3af3ce257c59ce139fc2cc25a37a49
JmdRodrigues <rjoao06@gmail.com>
2019-10-31 15:47:36 +0000

    added pdf reort

--- GrammarofTime/SignalAnnotation/annotation_feature.py
+++ GrammarofTime/SignalAnnotation/annotation_feature.py
@@ -5,50 +5,68 @@ from GrammarofTime.SSTS.backend.gotstools import *
 from PDF_generator.reportGen import Report
 from sklearn.neighbors import KernelDensity
 from definitions import CONFIG_PATH
-from tools.plot_tools import  plot_config, font_config
+from tools.plot_tools import  plot_config, font_config, subplot_pars
+from pandas import read_json
 
+guide = CONFIG_PATH + "/Hui_SuperProject/MovDict.json"
+guide_dict = read_json(guide).to_dict()
 example_path = CONFIG_PATH + "/Hui_SuperProject/Data_Examples/"
-signal = loadH5(example_path+"arthrokinemat_2018_06_02_23_51_55.h5")
+#pdf_config
+doc = Report("libphys", "report_AnnotationFiles")
+for key in guide_dict.keys():
 
-fs = 1000
-b = 10
-ch1 = signal[b*fs:, 5]
-
-# ch1 = (ch1-np.mean(ch1))/np.std(ch1)
-ch1_pp = pre_processing(ch1, "M LP 2")
-t = np.linspace(0, len(ch1_pp)/1000, len(ch1_pp))
-
-hist, bins = np.histogram(ch1_pp, bins=6)
-pd_s = prob_hist(ch1_pp, hist, bins, inverted=False, log=True)
-
-#plot config
-plot_config()
-font0, font1, font2 = font_config()
-
-# for windown_len in [10, 100, 250, 500, 1000]:
-windown_len = 10
-proc_ch1 = NewWindowStat(ch1_pp, ["std", "mean", "zcr"], 1000, windown_len)
-proc_ch1_norm = stat_white(proc_ch1)
-
-total = np.sum(abs(proc_ch1_norm), axis=1)
+    signal = loadH5(example_path+guide_dict[key]["file"])
 
 
-fig = plt.figure()
-plt.plot(total)
-plt.plot(ch1_pp)
-plt.plot(total*pd_s)
-plt.title("Test win_" + str(windown_len), fontproperties=font2)
-plt.show()
+    fs = 1000
+    b = 10
+    # for i in range(5, 8):
+    ch1 = signal[b * fs:(len(signal)//(guide_dict[key]["divider"]//2))+b*fs, 7]
+
+    # ch1 = (ch1-np.mean(ch1))/np.std(ch1)
+    ch1_pp = pre_processing(ch1, "M LP 2")
+    t = np.linspace(0, len(ch1_pp) / 1000, len(ch1_pp))
+
+    hist, bins = np.histogram(ch1_pp, bins=6)
+    pd_s = prob_hist(ch1_pp, hist, bins, inverted=False, log=True)
+
+    # plot config
+    plot_config()
+    font0, font1, font2 = font_config()
+
+    # for windown_len in [10, 100, 250, 500, 1000]:
+    windown_len = 500
+    proc_ch1 = NewWindowStat(ch1_pp, ["std", "mean", "Azcr"], 1000, windown_len)
+    proc_ch1_norm = stat_white(proc_ch1)
+
+    total = np.sum(abs(proc_ch1_norm), axis=1)
+
+    subplot_cenas = subplot_pars()
+
+    fig = plt.figure(subplotpars=subplot_cenas)
+    width, height = fig.get_size_inches()
+    fig.set_size_inches([width, height*3])
+    ax1 = plt.subplot(3,1,1)
+    ax1.plot(proc_ch1_norm)
+
+    ax2 = plt.subplot(3,1,2)
+    ax2.plot(ch1/max(ch1))
+    ax2.plot(pd_s)
+
+    ax3 = plt.subplot(3,1,3)
+    ax3.plot(total*pd_s)
+    plt.title(guide_dict[key]["pattern"], fontproperties=font2)
 
 
+    doc.add_title(
+        "Results with win_len="+str(windown_len)
+    )
+    doc.add_graph(fig, "Annotation of events with features", "annotation_testFeature_"+guide[key]["file"])
 
-# #pdf_config
-# doc = Report("libphys", "report_testWindows")
-# doc.add_title(
-#     "Results with win_len="+str(windown_len)
-# )
-# doc.add_graph(fig, "Annotation of events with feature windows of "+str(windown_len), "annotation_testFeature&PD_win_"+str(windown_len))
-#
-# print("...printing pdf")
-# doc.gen_pdf()
+print("...printing pdf")
+doc.gen_pdf()
+
+    # plt.show()
+
+
 
